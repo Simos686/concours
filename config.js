da1e4e768f6f8a217e460e1e29593dbd38f59fd5
Simos686 <145595046+Simos686@users.noreply.github.com>
2026-01-30 01:34:12 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // ⚙️ CONFIGURATION SUPABASE - CHANGEZ CES VALEURS !
 window.SUPABASE_URL = 'https://ehmbirsttpmudzejevto.supabase.co';  // Remplacez par VOTRE URL
-window.SUPABASE_ANON_KEY = 'sb_publishable_WWsqAb2gVHLMusu428_YCQ_r7OQ7iWs'; // Remplacez par VOTRE CLÉ
+window.SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVobWJpcnN0dHBtdWR6ZWpldnRvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk3MzEzNTYsImV4cCI6MjA4NTMwNzM1Nn0.DQb5Dxz7GKR40hwsNmGEEt5GjO4mEGBc_GYkd-9uuA4'; // Remplacez par VOTRE CLÉ
 
 // Tables
 window.DB_TABLES = {
@@ -12,3 +12,4 @@ window.DB_TABLES = {
 };
 
 console.log('⚙️ Configuration Supabase chargée');
+
